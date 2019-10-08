e3e978cc81118d1541a822bdc7a4d7ade38aa86b
Oliver Drozdowski <Oliver.Drozdowski@pernod-ricard.com>
2019-10-08 08:29:31 -0400

    Added some styling. Testing stuff

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Button from '@material-ui/core/Button'
+import './index.css'
 
 class App extends React.Component {
   render() {
-    return <div className="main">hello vgbay</div>;
+    return (
+      <div className="main">
+        <div className="header">
+          <AppBar className="header"> 
+            <Toolbar position="static">
+              <Button> test </Button>
+            </Toolbar>
+          </AppBar>
+        </div>
+      </div>
+    )
   }
 }
 
