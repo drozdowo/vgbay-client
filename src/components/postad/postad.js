c9f38f2606fb52df5199b26807b3895afaf34a63
Oliver Drozdowski <drozdowo@uwindsor.ca>
2019-11-13 21:25:02 -0500

    Added table for categories so its not user input anymore. Will be used for filtering

--- src/components/postad/postad.js
+++ src/components/postad/postad.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { TextField, Button } from "@material-ui/core";
+import {
+  TextField,
+  Button,
+  MenuItem,
+  FormControl,
+  InputLabel,
+  Select
+} from "@material-ui/core";
 import Axios from "axios";
 
 /**
@@ -12,15 +19,20 @@ class PostAd extends React.Component {
       category: "",
       name: "",
       description: "",
-      price: ""
+      price: "",
+      categories: []
     };
   }
+
+  componentWillMount = async () => {
+    this.getCategories();
+  };
 
   postAd = async () => {
     let result = await Axios.post(
       "http://localhost:3000/user/createad",
       {
-        category: this.state.category,
+        category: this.state.categories[this.state.category + 1].category,
         name: this.state.name,
         description: this.state.description,
         price: this.state.price
@@ -30,26 +42,54 @@ class PostAd extends React.Component {
     console.log(result);
   };
 
+  getCategories = async () => {
+    let result = await Axios.get("http://localhost:3000/search/categories");
+    if (result.status === 200 && result.data.dataType == "categories") {
+      this.setState({ categories: result.data.data });
+    }
+  };
+
   handleChange = evt => {
     let field = evt.target.name;
     let val = evt.target.value;
     let state = {};
     state[field] = val;
     this.setState(state);
-    console.log("state", this.state);
+  };
+
+  formatCategories = () => {
+    let categories = this.state.categories;
+    let menuItems = [];
+    for (let i = 0; i < categories.length; i++) {
+      let categoryObj = categories[i];
+      menuItems.push(
+        <MenuItem key={categoryObj.id} value={categoryObj.id}>
+          {categoryObj.category}
+        </MenuItem>
+      );
+    }
+    return menuItems;
+  };
+
+  setCategory = evt => {
+    console.log(evt.target);
+    this.setState({ category: evt.target.value });
   };
 
   render = () => {
     return (
       <div className="component">
         <h1 className="welcome"> Post a new ad </h1>
-        <TextField
-          name="category"
-          title="Category"
-          label="Category"
-          variant="outlined"
-          onChange={this.handleChange}
-        ></TextField>
+        <FormControl className="categorySelect">
+          <InputLabel id="label">Category</InputLabel>
+          <Select
+            id="demo-simple-select"
+            value={this.state.category}
+            onChange={this.setCategory}
+          >
+            {this.formatCategories()}
+          </Select>
+        </FormControl>
         <TextField
           name="name"
           title="Name"
